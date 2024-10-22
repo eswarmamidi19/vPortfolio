b5986709c97cca3926e9da57e3fe63812a02e321
eswarmamidi19 <eswarmamidi19@gmail.com>
2024-10-22 20:13:29 +0530

    setting layout

--- app/page.tsx
+++ app/page.tsx
@@ -9,27 +9,59 @@ import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
-// import {
-//   IconBrandGithub,
-//   IconBrandX,
-//   IconExchange,
-//   IconHome,
-//   IconNewSection,
-//   IconTerminal2,
-// } from "@tabler/icons-react";
-// import { FloatingDock } from "@/components/ui/floating-dock";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import { cn } from "@/lib/utils";
+import {
+  IconBrandGithub,
+  IconBrandX,
+  IconExchange,
+  IconHome,
+  IconNewSection,
+  IconTerminal2,
+} from "@tabler/icons-react";
 
 const Home = () => {
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+
+    {
+      title: "Projects",
+      icon: (
+        <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Components",
+      icon: (
+        <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Aceternity UI",
+      icon: <img className="" />,
+      href: "#",
+    },
+  ];
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         {/* <FloatingNav navItems={navItems} /> */}
-        {/* <FloatingDock
-          mobileClassName="translate-y-20" // only for demo, remove for production
-          desktopClassName="translate-y-20 z-10"
+        <FloatingDock
+          mobileClassName="" // only for demo, remove for production
+          desktopClassName={cn("fixed z-[99999] top-20 left-[40%] opacity-80")}
           items={links}
-        /> */}
+        />
+        {/* <h1 className="bg-red-700 text-3xl w-full absolute top-40 z-10">
+          Hello{" "}
+        </h1> */}
         <Hero />
         {/* <FloatingNav navItems={navItems} /> */}
         <Grid />
