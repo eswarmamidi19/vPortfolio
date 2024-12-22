7f77acb2bf23e2e42e9f3a7f5e17a10b4e2dbd12
Viswanathnani12 <viswanathnani12@gmail.com>
2024-12-22 12:31:13 +0530

    removed navbar in project page

--- app/(group1)/layout.tsx
+++ app/(group1)/layout.tsx
@@ -16,15 +16,15 @@ type LayoutProps = {
   children: React.ReactNode;
 };
 export default function ProjectLayouts({ children }: LayoutProps) {
-  const navItems = [
-    { name: "Home", link: "/" },
-    { name: "Projects", link: "#projects" },
-    { name: "Approach", link: "#approach" },
-    { name: "Contact", link: "#contact" },
-  ];
+  // const navItems = [
+  //   { name: "Home", link: "/" },
+  //   { name: "Projects", link: "#projects" },
+  //   { name: "Approach", link: "#approach" },
+  //   { name: "Contact", link: "#contact" },
+  // ];
   return (
     <>
-      <FloatingNav navItems={navItems} />
+      {/* <FloatingNav navItems={navItems} /> */}
       <div
         className=" pb-40 md:pb-0 pt-10  md:pt-36 px-4 md:px-5 w-screen  flex flex-col    
       [mask-image:radial-gradient(ellipse_at_center,transparent_20%]"
