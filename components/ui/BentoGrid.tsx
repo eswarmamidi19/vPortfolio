e874a3f881892c040fc855d848b1f10f334f4ecf
eswarmamidi19 <eswarmamidi19@gmail.com>
2024-12-27 00:09:08 +0530

    fix : magic button in bento

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -140,15 +140,23 @@ export const BentoGridItem = ({
           {id === 2 && <GridGlobe />}
 
           {id === 4 && (
-            <div className="h-[20%] w-[20%]">
-              <a href="https://medium.com/@bvarungupta" target="_blank">
-                <MagicButton
-                  title="Read Now"
-                  icon={<FaLocationArrow />}
-                  position="left"
-                  handleClick={handleCopy}
-                  otherClasses="!bg-[#161A31]"
-                />
+            <div className="h-[20%] w-full absolute">
+              <a
+                href="https://medium.com/@bvarungupta"
+                target="_blank"
+                className=" relative top-16  bg-white-100 "
+              >
+                <button className="relative inline-flex h-12  md:w-50 md:mt-10 xl:w-60 overflow-hidden rounded-lg p-[1px] focus:outline-none">
+                  <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+
+                  {/* remove px-3 py-1, add px-5 gap-2 */}
+                  <span
+                    className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
+             bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 `}
+                  >
+                    read more
+                  </span>
+                </button>
               </a>
             </div>
           )}
